dbd9077b23bc650e2be5010fc102552cb7470f80
Claes Persson <c@cp.se>
2017-08-05 16:46:15 +0200

    getting window size/cursor position - displaying rows

--- src/cg.c
+++ src/cg.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -9,14 +10,81 @@
 
 #define KEY_CTRL(k) ((k) & 0x1f)
 
+struct config {
+	int rows;
+	int cols;
+	struct termios init_term;
+};
+
 const char *VERSION = "0.0.1";
-struct termios bkterm;
+struct config ECFG;
 
 void
 die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
 	perror(s);
 	exit(1);
+}
+
+void
+draw_rows(void)
+{
+	int y = 0;
+	for (y = 0; y < ECFG.rows; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+int
+get_cursor_pos(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+		if (buf[i] == 'R')
+			break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[')
+		return -1;
+
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return -1;
+
+	return 0;
+}
+
+int
+get_win_size(int *rows, int *cols)
+{
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+			return -1;
+		return get_cursor_pos(rows, cols);
+	} else {
+		*rows = ws.ws_row;
+		*cols = ws.ws_col;
+		return 0;
+	}
+}
+
+void
+init_editor(void)
+{
+	if (get_win_size(&ECFG.rows, &ECFG.cols) == -1)
+		die("get_win_size");
 }
 
 void
@@ -26,6 +94,8 @@ input(void)
 
 	switch (c) {
 		case KEY_CTRL('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
 	}
@@ -54,17 +124,17 @@ main(int argc, char *argv[])
 void
 rawmode_off(void)
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &bkterm);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &ECFG.init_term);
 }
 
 void
 rawmode_on(void)
 {
-	if (tcgetattr(STDIN_FILENO, &bkterm) == -1)
+	if (tcgetattr(STDIN_FILENO, &ECFG.init_term) == -1)
 		die("tcgetattr");
 	atexit(rawmode_off);
 
-	struct termios r = bkterm;
+	struct termios r = ECFG.init_term;
 	r.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	r.c_oflag &= ~(OPOST);
 	r.c_cflag |= (CS8);
@@ -93,6 +163,7 @@ read_key(void)
 void
 read_loop(void)
 {
+	init_editor();
 	rawmode_on();
 
 	while(1) {
@@ -106,11 +177,16 @@ refresh(void)
 {
 	write(STDOUT_FILENO, "\x1b[2J", 4); /* clear screen */
 	write(STDOUT_FILENO, "\x1b[H", 3);  /* move cursor to top-left */
+
+	draw_rows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 void
 usage(char *name)
 {
 	printf("usage: %s [-v]\n", name); 
+	exit(0);
 }
 
--- src/cg.h
+++ src/cg.h
@@ -1,4 +1,8 @@
 void die(const char *s);
+void draw_rows(void);
+int get_cursor_pos(int *rows, int *cols);
+int get_win_size(int *rows, int *cols);
+void init_editor(void);
 void input(void);
 int main(int argc, char *argv[]);
 void rawmode_off(void);
