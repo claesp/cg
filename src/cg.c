eec0d979c4c78ba6a34e2dda92829225fd4a679e
Claes Persson <c@cp.se>
2017-08-05 15:33:43 +0200

    skeleton work, terminal setup

--- src/cg.c
+++ src/cg.c
@@ -1,8 +1,71 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "cg.h"
 
 char *VERSION = "0.0.1";
+struct termios bkterm;
 
-int main(int argv, char *argc[])
+void
+d_rawmode(void)
 {
-	printf("cg %s\n", VERSION);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &bkterm);
 }
+
+void
+e_rawmode(void)
+{
+	tcgetattr(STDIN_FILENO, &bkterm);
+	atexit(d_rawmode);
+
+	struct termios r = bkterm;
+	r.c_iflag &= ~(ICRNL | IXON);
+	r.c_oflag &= ~(OPOST);
+	r.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &r);
+}
+
+void
+input(void)
+{
+	e_rawmode();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	int vflag = 0;
+	int c = 0;
+	while ((c = getopt(argc, argv, "v")) != -1) {
+		switch (c) {
+			case 'v':
+				vflag = 1;
+				break;
+			default:
+				usage(argv[0]);
+		}
+	}
+
+	input();
+
+	return 0;
+}
+
+void
+usage(char *name)
+{
+	printf("usage: %s [-v]\n", name); 
+}
+
---
+++ src/cg.h
@@ -0,0 +1,5 @@
+void d_rawmode(void);
+void e_rawmode(void);
+void input(void);
+int main(int argc, char *argv[]);
+void usage(char *name);
